8e76051e69f9af7af631b0c400c3fa5386e8f4fb
NozomuTsuruta <tuno.0806@icloud.com>
2021-03-20 16:04:22 +0900

    sqliteの例追加

---
+++ db-example/sqlite3/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main()  {
+	db, err := sql.Open("sqlite3", "./foo.db")
+	checkError(err)
+
+	// 挿入
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkError(err)
+	res, err := stmt.Exec("nozomu", "アルバイト", "2020-08-01")
+	checkError(err)
+
+	id, err := res.LastInsertId()
+	checkError(err)
+
+	fmt.Println(id)
+
+	// 更新
+	stmt, err = db.Prepare("UPDATE userinfo SET username=? WHERE uid=?")
+	checkError(err)
+
+	res,err = stmt.Exec("nozomuupdate", id)
+	checkError(err)
+
+	affect, err := res.RowsAffected()
+	checkError(err)
+
+	fmt.Println(affect)
+
+	// 検索
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkError(err)
+
+	for rows.Next() {
+		var uid int
+        var username string
+        var department string
+        var created time.Time
+        err = rows.Scan(&uid, &username, &department, &created)
+        checkError(err)
+        fmt.Println(uid)
+        fmt.Println(username)
+        fmt.Println(department)
+        fmt.Println(created)
+	}
+
+	// 削除
+	stmt, err = db.Prepare("DELETE FROM userinfo WHERE uid=?")
+	checkError(err)
+
+	res, err = stmt.Exec(id)
+	checkError(err)
+
+	affect, err = res.RowsAffected()
+	checkError(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+}
+
+func checkError(err error)  {
+	if err != nil {
+		panic(err)
+	}
+}
